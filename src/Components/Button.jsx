eac9e34361ebc2432f516df7b4f360a1b26f046d
Rabeet-5 <rabeetsati@gmail.com>
2024-03-18 02:19:18 +0500

    update

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ children }) => {
     return (
-        <button className=' bg-blue-100 px-4 py-2  rounded-xl shadow-2xl'>{children}</button>
+        <button className=' text-wrap bg-blue-100 px-4 py-2  rounded-xl shadow-2xl'>{children}</button>
     )
 }
 
